1f48f7751d864fc029f6e08721e5ebf6461e35ab
paulotruly <paulomsmsms@gmail.com>
2025-09-30 11:48:09 -0300

    arrumandodesign

--- simple-blog/src/app/main/main.ts
+++ simple-blog/src/app/main/main.ts
@@ -29,13 +29,13 @@ export class Main {
     {
       title: 'The revolution of independent sound taking over the music scene',
       content: 'Over the past few years, Indietronica has sparked a revolution in the way we perceive music. What started as a niche subgenre blending indie rock and electronic beats has now become the heartbeat of independent music. Artists are no longer confined by traditional genre boundaries; instead, they are merging organic sounds with digital innovation, creating an entirely new auditory experience. This article delves into how INDIETRONICA is shaping the future of music, how it’s breaking free from mainstream formulas, and why this genre is the perfect soundtrack for the modern, digitally-driven world.',
-      img: 'https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/8c/76/4e/8c764e2b-a24e-d48d-b225-8162fde772d5/191018258931.jpg/1200x630bb.jpg',
+      img: 'https://i-d.co/wp-content/uploads/2024/05/1690283272698-pc-music3.png?quality=90&w=1920',
       postDate: new Date('2025-09-29T10:00:00')
     },
     {
       title: 'My favorite albums of 2025',
       content: 'My favorite albums of 2025 include Eusexua by FKA twigs, which combines haunting production with raw emotion, exploring themes of desire and transformation; choke enough by Oklou, a dreamy and melancholic album balancing electronic minimalism and emotional depth; Coisas Naturais by Marina Sena, a vibrant mix of Brazilian pop, MPB, and funk that showcases her unique style; and Addison by Addison Rae, a surprising and introspective pop debut that blends playful energy with maturity. These albums have shaped my 2025 soundtrack and brought a lot of variety and feeling to the year.',
-      img: 'https://images.impresa.pt/expresso/2025-02-17-jhe_240214_fka_twigs_promo_04_048r1_t.jpg-abde71bf/original',
+      img: 'https://www.nme.com/wp-content/uploads/2025/06/nme-best-albums-of-2025-so-far.jpg',
       postDate: new Date('2025-09-28T18:30:00')
     }
   ]);
